99257ff521ca9184ed77eac5cd08444b84916abf
Zabith Fulail <muhammadh_s@epiclanka.net>
2024-03-05 08:24:26 +0530

    minor changes

--- lib/features/presentation/views/splash/splash_view.dart
+++ lib/features/presentation/views/splash/splash_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import '../login/login_view.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -12,31 +13,37 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   @override
   void initState() {
     super.initState();
-    Timer(const Duration(seconds: 5), () => Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(builder: (context) => const LoginView()), // Replace with your login view widget
-    ));
+    Timer(
+        const Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      const LoginView()), // Replace with your login view widget
+            ));
   }
 
   @override
   Widget build(BuildContext context) {
-    return     AnnotatedRegion(
+    return AnnotatedRegion(
       value: SystemUiOverlayStyle.dark,
       child: Scaffold(
         body: Container(
-          decoration:  const BoxDecoration(
-              gradient:  LinearGradient(
-                colors: [Colors.white, Color(0xFFE7E7E7)],
-                stops: [0.0, 1.0],
-                begin: FractionalOffset.topCenter,
-                end: FractionalOffset.bottomCenter,
-              )),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.white, Color(0xFFE7E7E7)],
+            stops: [0.0, 1.0],
+            begin: FractionalOffset.topCenter,
+            end: FractionalOffset.bottomCenter,
+          )),
           child: const Center(
-            child: Icon(Icons.ac_unit,color: Colors.deepOrangeAccent,),
+            child: Icon(
+              Icons.ac_unit,
+              color: Colors.deepOrangeAccent,
+            ),
           ),
         ),
       ),
