2f00c4a69868365f7e51dd6e0307524a54fcf10b
AbdhulFathir <abdhulfathirforepic>
2024-03-07 23:47:47 +0530

    Added Routes

--- lib/features/presentation/views/home_view/home_view.dart
+++ lib/features/presentation/views/home_view/home_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:gem_book/units/app_strings.dart';
 import 'package:sizer/sizer.dart';
 import '../../../../units/app_colors.dart';
 import '../../../../units/app_images.dart';
-
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
@@ -21,29 +21,33 @@ class _HomeViewState extends State<HomeView> {
       drawer: Drawer(
         child: drawerView(),
       ),
-      appBar: AppBar(
-        backgroundColor: AppColors.baseColor,
-        title: const Center(child: Text("Gem Home")),
-        leading: GestureDetector(
-          child: SvgPicture.asset(
-            AppImages.iconMenu,
-            width: 0.5.w,
+      // appBar: AppBar(
+      //   backgroundColor: AppColors.baseColor,
+      //   title: const Center(
+      //       child: Text(
+      //     AppStrings.homeTitle,
+      //     style: TextStyle(color: Colors.white),
+      //   )),
+      //   leading: GestureDetector(
+      //     child: SvgPicture.asset(
+      //       AppImages.iconMenu,
+      //       width: 0.5.w,
+      //     ),
+      //     onTap: () {
+      //       _scaffoldKey.currentState!.openDrawer();
+      //     },
+      //   ),
+      // ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 17.h),
+          child: SizedBox(
+            width: double.infinity,
+            height: double.infinity,
+          child: Column(),
           ),
-          onTap: () {
-            _scaffoldKey.currentState!.openDrawer();
-          },
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
-          width: double.infinity,
-          child: const Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[],
-          ),
-        ),
+        )
+
       ),
     );
   }
--- lib/units/app_strings.dart
+++ lib/units/app_strings.dart
@@ -11,6 +11,7 @@ class AppStrings {
   static const String introDescription ="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse";
   static const String register = "Register";
   static const String forgotPassword = "Forgot password";
+  static const String homeTitle = "Gem Home";
 
 
 
