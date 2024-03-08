98d15688e44c30d58649e5ac692ccc05e6ef2949
AbdhulFathir <abdhulfathirforepic>
2024-03-09 01:26:30 +0530

    Added Drawer

--- lib/features/presentation/views/intro/intro_page.dart
+++ lib/features/presentation/views/intro/intro_page.dart
@@ -5,57 +5,52 @@ import 'package:gem_book/units/app_strings.dart';
 import 'package:sizer/sizer.dart';
 import '../../../../units/routes.dart';
 
-class IntroPage extends StatelessWidget {
+class IntroPage extends StatefulWidget {
   const IntroPage({super.key});
 
   @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  SizedBox(height: 5.h),
-                  Container(
-                    height: 40.h,
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                          AppImages.intoImg,
-                        ),
-                      ),
-                    ),
+            SizedBox(height: 5.h),
+            Container(
+              height: 40.h,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    AppImages.intoImg,
                   ),
-                  const Text(
-                    "${AppStrings.welcome} ${AppStrings.to}\n${AppStrings.gemBook}",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 40,
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  const Text(
-                    AppStrings.introDescription,
-                    textAlign: TextAlign.center,
-                  ),
-                ],
+                ),
               ),
             ),
+            const Text(
+              "${AppStrings.welcome} ${AppStrings.to}\n${AppStrings.gemBook}",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 40,
+              ),
+            ),
+            SizedBox(height: 5.h),
+            const Text(
+              AppStrings.introDescription,
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 5.h),
             BtnComponent(
               title: AppStrings.login,
               color: Colors.grey,
               onTap: () {
                 Navigator.popUntil(context, ModalRoute.withName(Routes.kLoginView));
               },
-            ),
-            const SizedBox(
-              height: 8,
             ),
             BtnComponent(
               title: AppStrings.register,
---
+++ lib/units/app_styling.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AppStyling {
+  static const TextStyle boldTextSize20 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static const TextStyle normalTextSize14 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 14,
+  );
+
+  static const TextStyle bold700TextSize20 = TextStyle(
+    fontWeight: FontWeight.w700,
+    fontSize: 20,
+  );
+
+  static const TextStyle bold500TextSize16 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+}
