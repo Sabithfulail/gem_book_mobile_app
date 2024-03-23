cd08e8a028e6b9437ab5997b268cf97e10e16376
AbdhulFathir <abdhulfathirforepic>
2024-03-23 15:11:36 +0530

    done Edit post

--- lib/features/services/database_service.dart
+++ lib/features/services/database_service.dart
@@ -13,21 +13,34 @@ class DatabaseService {
         fromFirestore: (snapshots, _) => Add.fromJson(snapshots.data()!),
         toFirestore: (add, _) => add.tojson());
   }
-  Stream<QuerySnapshot> getAdds(){
+
+  Stream<QuerySnapshot> getAdds() {
     return addRef.snapshots();
   }
 
   void addAGemAdd(Add add) async {
     final DocumentReference docRef = await addRef.add(add);
-    addRef.add(add);
     await docRef.update({'addID': docRef.id});
   }
 
-  void updateAdd(String addID,Add add){
-    addRef.doc(addID).update(add.tojson());
+  Future<void> updateAdd(String addID, Add add) async {
+    addRef.doc(addID).update({
+      'imageGem': add.imageGem,
+      'imageCerti': add.imageCerti,
+      'name': add.name,
+      'price': add.price,
+      'shape': add.shape,
+      'type': add.type,
+      'colour': add.colour,
+      'details': add.details,
+      'weight': add.weight,
+    });
+    // addRef.doc(addID).delete();
+    // final DocumentReference docRef = await addRef.add(add);
+    // await docRef.update({'addID': docRef.id});
   }
 
-  void deleteAdd(String addID){
+  void deleteAdd(String addID) {
     addRef.doc(addID).delete();
   }
 }
